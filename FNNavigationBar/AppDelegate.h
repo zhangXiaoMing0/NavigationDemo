afe0653f195ec67be6518cf384acc86cc2c54760
zhangXiaoMing0 <zmx@zmx-macbook-pro.local>
2016-01-24 10:16:54 +0800

    Initial Commit

---
+++ FNNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FNNavigationBar
+//
+//  Created by zmx on 16/1/24.
+//  Copyright © 2016年 zmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FNNavigationBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FNNavigationBar
+//
+//  Created by zmx on 16/1/24.
+//  Copyright © 2016年 zmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
